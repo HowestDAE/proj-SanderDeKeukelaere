e5093f2c39aea4d6be52c3e96c2bb4ff3c970116
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-03-15 08:51:50 +0100

    Replaced description with class symbols
    Added ClassToIcon converter that converts string with class name to the icon image
    Replaced the description of each item (sometimes it gets way too long) with the class icons that the item is being used by

---
+++ Project_TF2ItemList/View/Converters/ClassToIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Project_TF2ItemList.View.Converters
+{
+    internal class ClassToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new BitmapImage(new Uri($"pack://Application:,,,/Resources/ClassSymbols/{value}.png"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
