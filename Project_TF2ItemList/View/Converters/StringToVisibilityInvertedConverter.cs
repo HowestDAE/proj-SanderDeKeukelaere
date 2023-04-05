8ea689780d53a986ea785d98653a22084da2991e
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-04-05 23:26:54 +0200

    Added messages if something is not applicable
    If a property is not applicable (e.g. holiday restriction) it will now show a small text (e.g. this item has no holiday restriction)

---
+++ Project_TF2ItemList/View/Converters/StringToVisibilityInvertedConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Project_TF2ItemList.View.Converters
+{
+    public class StringToVisibilityInvertedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && ((string)value).Length > 0)
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
