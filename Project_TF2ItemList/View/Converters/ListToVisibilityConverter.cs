b60a3f150b8edbc8ae6a4223cb74cacaa16cce86
Sander De Keukelaere <dekeukelaeresander@gmail.com>
2023-04-14 11:21:31 +0200

    Added missing texts if a property wasn't used
    Added "Can not be equiped" and "Does not belong to a class" texts when no ItemSlot or Classes are assigned to an item
    Changed disabled ListBoxes to stackpanels

---
+++ Project_TF2ItemList/View/Converters/ListToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Project_TF2ItemList.View.Converters
+{
+    public class ListToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && ((List<string>)value).Count > 0)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
